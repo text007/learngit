ef0087a96c6dc99b31688daae19035651bb8519a
jzheng <jzheng@elifegames.com>
2019-07-19 18:53:50 +0800

    add more files

--- 4、运算符.py
+++ 4、运算符.py
@@ -212,3 +212,19 @@ else:
    print ("not(a and b) 都为 true,", not( a and b ))
    
 print('------------------------------')
+
+
+# 成员运算符
+'''
+in	如果在指定的序列中找到值返回 True，否则返回 False。
+not in	如果在指定的序列中没有找到值返回 True，否则返回 False。
+'''
+
+a = 10
+b = 20
+list1 = [1, 2, 3, 4, 5]
+
+if (a in list1)
+    print('a 在 list1 中')
+else:
+    print('a 不足 list1 中')
